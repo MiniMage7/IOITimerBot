81e97c26dd9286dc292ed9121caa9243f469994c
Winter <156695913+WThomas88@users.noreply.github.com>
2024-02-12 16:07:23 -0600

    Updating readme
    Also clarified some add_role and remove_role messages

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # IOITimerBot
-Discord bot that keeps track and notifies users when puzzles refresh
+#### Discord bot that keeps track and notifies users when puzzles refresh
+
+## Important Information
+A few things of the bot are hard coded. The bot will only currently function in the IOI fan server and a test server. The channels that the bot sends it "Puzzle has refreshed!" are set whenever the bot starts from channels.json. The roles.json and embedMessages.json store the pingable role ids and embed messages so that if the bot turns off, all the information is stored and it can restart without any issues. You should never have to mess with these.
+
+## Normal Commands
+
+### $help_me
+Sends a message in chat that provides some information about the following commands.
+
+### $add_role and $remove_role
+Adds or removes a pingable role from the user. (If the user wants to be pinged for Verdant Glen Hidden Cubes, they would do `$add_role "Verdant Glen" "Hidden Cubes"`)  
+Doing just `$add_role` or `$remove_role` will explain how to use the command.  
+Doing `$add_role` with an invalid area i.e. `$add_role "test" "test"` will print out all the valid areas.  
+Doing `$add_role` with a valid area but invalid puzzle will list all puzzles that can be done for the area. i.e. `$add_role "Verdant Glen" "test"`  
+If there are no users with a pingable role after `$remove_role` is called, it is deleted. It will be remade if someone else calls `$add_role` for the role again.
--- main.py
+++ main.py
@@ -236,7 +236,7 @@ async def add_role(ctx, *args):
         await ctx.channel.send(f"{ctx.author.mention}\n"
                                f"This command is used to add a pingable role to yourself for a specific puzzle.\n"
                                f"`$add_role \"area\" \"puzzle\"`\n"
-                               f"The quotes are necessary.")
+                               f"The quotes and capitalization are necessary.")
         return
 
     # Assign the arguments
@@ -247,14 +247,14 @@ async def add_role(ctx, *args):
     if area not in puzzleAreas:
         await ctx.channel.send(f"{ctx.author.mention}\nThat is not a valid area. Valid areas are: \"Verdant Glen\", "
                                f"\"Lucent Waters\", \"Autumn Falls\", \"Shady Wildwood\", and \"Serene Deluge\"."
-                               f"\nRemember to use the quotes.")
+                               f"\nRemember quotes and capitalization.")
         return
 
     # Check that the puzzle is in the area
     if puzzle not in acceptedPuzzles[area]:
         await ctx.channel.send(f"{ctx.author.mention}\nThat is not a valid area.\n"
                                f"Valid areas for {area} are: {acceptedPuzzles[area]}\n"
-                               f"Remember to use double quotes around the puzzle name.")
+                               f"Remember to use double quotes and capitalization.")
         return
 
     # Check if the role already exists
@@ -287,7 +287,7 @@ async def remove_role(ctx, *args):
         await ctx.channel.send(f"{ctx.author.mention}\n"
                                f"This command is used to remove a pingable role from yourself.\n"
                                f"`$remove_role \"area\" \"puzzle\"`\n"
-                               f"The quotes are necessary.")
+                               f"Quotes and capitalization are necessary.")
         return
 
     # Assign the arguments
@@ -298,14 +298,14 @@ async def remove_role(ctx, *args):
     if area not in puzzleAreas:
         await ctx.channel.send(f"{ctx.author.mention}\nThat is not a valid area. Valid areas are: \"Verdant Glen\", "
                                f"\"Lucent Waters\", \"Autumn Falls\", \"Shady Wildwood\", and \"Serene Deluge\"."
-                               f"\nRemember to use the quotes.")
+                               f"\nRemember to use double quotes and capitalization.")
         return
 
     # Check that the puzzle is in that area
     if puzzle not in acceptedPuzzles[area]:
         await ctx.channel.send(f"{ctx.author.mention}\nThat is not a valid area.\n"
                                f"Valid areas for {area} are: {acceptedPuzzles[area]}\n"
-                               f"Remember to use double quotes around the puzzle name.")
+                               f"Remember to use double quotes and capitalization.")
         return
 
     # Make sure the role exists
